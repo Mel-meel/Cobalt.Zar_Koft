989982424ff0334d71e6e09029eb2374937e8dc0
Mel-meel <mel.avizou@mail.ru>
2023-07-29 23:49:19 +0200

    Correction bugs

--- Mysql.py
+++ Mysql.py
@@ -4,10 +4,12 @@ class Mysql :
 
     # Constructeur de la classe
     def __init__(self, configs) :
-        self.url = url
-        self.soup = ""
-        mydb = mysql.connector.connect(
-            host=configs[0],
-            user=configs[1],
-            password=configs[2]
-        )
+        try :
+            with connector.connect(
+                host = configs[0],
+                user = configs[1],
+                password = configs[2]
+            ) as connexion :
+            self.connexion = connexion
+        except connector.Error as e :
+            print(e)
